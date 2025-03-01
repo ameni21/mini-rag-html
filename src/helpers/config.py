f3d92bc7dc5c7355ea9a9f2aa642f29d943d4483
Ameni mhadhbi <91744121+ameni21@users.noreply.github.com>
2025-03-01 15:33:27 +0100

    models postgres

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,17 @@ class Settings(BaseSettings):
 
     APP_NAME: str
     APP_VERSION: str
+
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
+
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_MAIN_DATABASE: str
+
     
     class Config:
         env_file = ".env"
